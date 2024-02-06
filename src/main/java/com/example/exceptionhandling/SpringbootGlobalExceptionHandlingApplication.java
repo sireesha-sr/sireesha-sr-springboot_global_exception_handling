2ad23063fbc0b68137eec88cbe50df8e1845fde3
sireesha-sr <79186219+sireesha-sr@users.noreply.github.com>
2024-02-06 14:24:45 +0530

    Add files via upload

---
+++ src/main/java/com/example/exceptionhandling/SpringbootGlobalExceptionHandlingApplication.java
@@ -0,0 +1,13 @@
+package com.example.exceptionhandling;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootGlobalExceptionHandlingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootGlobalExceptionHandlingApplication.class, args);
+	}
+
+}
